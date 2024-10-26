33fd0e94fbb0466371ddc45b1491f447a099f487
tom <tomtom.tamisier@gmail.com>
2024-10-26 12:26:02 +0200

    add(sketch): lines.js

---
+++ sketches/lines.js
@@ -0,0 +1,67 @@
+margin = 40;
+
+function setup(){
+    createCanvas(660, 660);
+    background(227, 209, 185)
+    drawGrid();
+    strokeWeight(1);
+    textSize(15);
+    fill(0);
+    noStroke();
+    text("The grid", (width-margin)/2, 25);
+}
+
+function draw(){
+    
+}
+
+function drawGrid(){
+    //blending
+    noFill();
+    stroke(50);
+    rect(margin, margin, width - 2 * margin, height - 2 * margin);
+
+    height_offset = height-2*margin;
+    let lineCount = 0; // Counter to track every 4th line
+    for (let i = 0; i < height_offset; i += height_offset / 66) {
+        if (lineCount % 3 === 0) {
+            strokeWeight(1.5);
+        } else {
+            strokeWeight(0.5);
+        }
+        r = sin(random())*3
+        line(margin, margin + i+r, width - margin, margin + i+r);
+        lineCount++; // Increment the counter for each line drawn
+    }
+
+    width_offset = width-2*margin;
+    for (let i = margin; i < width_offset+margin; i += width_offset / 33) {
+        strokeWeight(0.5);
+        r = sin(random())*4
+        line(i+r, margin,i+r, height - margin);
+    }
+    strokeWeight(2);
+    beginShape();
+        vertex(margin, 0+10);
+        vertex(margin, margin);
+        vertex(10, margin);
+    endShape();
+
+    beginShape();
+        vertex(width-margin, 10);
+        vertex(width-margin, margin);
+        vertex(width-10, margin);
+    endShape();
+
+    beginShape();
+        vertex(margin, height-10);
+        vertex(margin, height-margin);
+        vertex(10, height-margin);
+    endShape();
+
+    beginShape();
+        vertex(width-margin, height-10);
+        vertex(width-margin, height-margin);
+        vertex(width-10, height-margin);
+    endShape();
+}
